0c73baa6dda0fc94908925196711744b6b148de4
iguto <clc.iguto@gmail.com>
2014-12-20 01:35:17 +0900

    annotate

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: ingredients
+#
+#  id          :integer          not null, primary key
+#  cocktail_id :integer
+#  material_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Ingredient < ActiveRecord::Base
 end
