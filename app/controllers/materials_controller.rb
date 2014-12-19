f8a44e3fc004501b2e5b288bda474c96b68bcc64
mizobuchi <s11t275@stmail.eng.kagawa-u.ac.jp>
2014-12-20 04:47:24 +0900

    materialの追加部分を作成

--- app/controllers/materials_controller.rb
+++ app/controllers/materials_controller.rb
@@ -2,4 +2,22 @@ class MaterialsController < ApplicationController
   def index
     @materials = Material.all
   end
+  def new
+    @material = Material.new
+  end
+  def create
+    @material = Material.new(material_params)
+    @material = nil unless @material.save
+    unless @material
+      render :new and return
+    end
+    redirect_to materials_path
+  end
+
+  private
+
+  def material_params
+    params.require(:material).permit(:name)
+  end
+
 end
