9b62fdc21d8f07e7b853d25be41e35ffde7aceb6
iguto <clc.iguto@gmail.com>
2014-12-20 02:01:27 +0900

    annotate

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,18 +2,17 @@
 #
 # Table name: cocktails
 #
-#  id            :integer          not null, primary key
-#  name          :string(255)      not null
-#  name_en       :string(255)
-#  taste         :string(255)
-#  technic       :string(255)
-#  tpo           :string(255)
-#  glass         :string(255)
-#  base_id       :integer
-#  ingredient_id :integer          not null
-#  volume        :integer
-#  created_at    :datetime
-#  updated_at    :datetime
+#  id         :integer          not null, primary key
+#  name       :string(255)      not null
+#  name_en    :string(255)
+#  taste      :string(255)
+#  technic    :string(255)
+#  tpo        :string(255)
+#  glass      :string(255)
+#  base_id    :integer
+#  volume     :integer
+#  created_at :datetime
+#  updated_at :datetime
 #
 
 class Cocktail < ActiveRecord::Base
