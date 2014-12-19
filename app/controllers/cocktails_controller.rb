b942746818a15b51e5187b009d9632d757bde818
iguto <clc.iguto@gmail.com>
2014-12-20 05:56:22 +0900

    カクテルの登録

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,4 +2,21 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
   end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save!
+
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :name_en, :taste, :technique, :tpo, :glass, :base, :volume)
+  end
 end
