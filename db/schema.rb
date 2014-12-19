b09f438f6e541c568eee9c3740e9f11afa0aa915
iguto <clc.iguto@gmail.com>
2014-12-20 01:32:30 +0900

    rails g model material

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219161547) do
+ActiveRecord::Schema.define(version: 20141219163206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20141219161547) do
     t.integer  "base_id"
     t.integer  "ingredient_id", null: false
     t.integer  "volume"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "materials", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
